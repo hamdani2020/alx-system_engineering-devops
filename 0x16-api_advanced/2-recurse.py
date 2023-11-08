4f41289fc0d80234a6e0605fe10dcba022c8c7d5
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2023-11-08 01:50:12 -0800

    recurse it

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-"""This API determines the subreddit sub count
-"""
-
+"""Query Reddit API to determine subred"""
 import requests
 
 
 def recurse(subreddit, hot_list=[], next_page=None, count=0):
-    """This method requests subreddit recursively using pagination
+    """Request subreddit recursively using pagination
     """
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     user_agent = '0x16-api_advanced-hamdani2020'
@@ -19,8 +17,10 @@ def recurse(subreddit, hot_list=[], next_page=None, count=0):
     if rq.status_code != 200:
         return None
 
+    # load response unit from json
     data = rq.json()['data']
 
+    # extract list of pages
     posts = data['children']
     for post in posts:
         count += 1
